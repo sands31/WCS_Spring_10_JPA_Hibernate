e16387d78f7d15ad1762ec676e29239163c4b732
Sandra Roques <roquesandra@hotmail.fr>
2019-11-19 16:47:07 +0100

    challenge done

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SchoolController {
 
     // TODO : get school repository by dependency injection
+	@Autowired
+    private SchoolRepository repository;
 
     @GetMapping("/schools")
     public String getAll(Model model) {
 
-        // TODO : find all schools
+    	model.addAttribute("schools", repository.findAll());
 
         return "schools";
     }
@@ -26,6 +32,14 @@ public class SchoolController {
                             @RequestParam(required = false) Long id) {
 
         // TODO : find one school by id
+    	 School school = new School();
+         if (id != null) {
+             Optional<School> optionalWizard = repository.findById(id);
+             if (optionalWizard.isPresent()) {
+                 school = optionalWizard.get();
+             }
+         }
+         model.addAttribute("school", school);
 
         return "school";
     }
@@ -34,7 +48,7 @@ public class SchoolController {
     public String postSchool(@ModelAttribute School school) {
 
         // TODO : create or update a school
-
+    	repository.save(school);	
         return "redirect:/schools";
     }
 
@@ -42,6 +56,7 @@ public class SchoolController {
     public String deleteSchool(@RequestParam Long id) {
 
         // TODO : delete a school
+    	repository.deleteById(id);
 
         return "redirect:/schools";
     }
